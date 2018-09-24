f3fb7e94ece3b4c8fd1beae8d8fd271124828dee
Fernando Herrera <fernando.herrera85@gmail.com>
2018-09-23 21:42:25 -0600

    modificamos el app.js ruta del SW

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 
+var url = window.location.href;
+var swLocation = '/twittor/sw.js';
+
 
 if ( navigator.serviceWorker ) {
-    navigator.serviceWorker.register('/sw.js');
+
+
+    if ( url.includes('localhost') ) {
+        swLocation = '/sw.js';
+    }
+
+
+    navigator.serviceWorker.register( swLocation );
 }
 
 
