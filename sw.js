3b3e7491a174eb38dea3fa409e238686706250b2
Fernando Herrera <fernando.herrera85@gmail.com>
2018-09-24 08:52:51 -0600

    modificaciones para ios

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE    = 'static-v2';
+const STATIC_CACHE    = 'static-v3';
 const DYNAMIC_CACHE   = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
